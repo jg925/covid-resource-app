eab8004cf331684994cd7cfc3f0166a6eff97382
Michelle Cheng <miche2lec@gmail.com>
2020-07-09 22:17:02 -0700

    Create initial project structure and started resources page

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+// app.dart
+
+import 'package:flutter/material.dart';
+import 'screens/resources/resources.dart';
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Resources(),
+    );
+  }
+}
