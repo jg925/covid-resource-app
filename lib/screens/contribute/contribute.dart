3854d7f11272b937b5a3ea2e4f526a3d125632c5
Jinny Gui <jg925@cornell.edu>
2020-07-16 17:40:59 -0400

    setup basic structure

--- lib/screens/contribute/contribute.dart
+++ lib/screens/contribute/contribute.dart
@@ -1,6 +1,7 @@
 // screens/contribute/contribute.dart
 
 import 'package:flutter/material.dart';
+import 'section.dart';
 
 class Contribute extends StatelessWidget {
   Contribute({this.color, this.title, this.onPush});
@@ -13,31 +14,19 @@ class Contribute extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            "Contribute Page",
+            "Ways You Can Help",
           ),
           backgroundColor: color,
         ),
-        body: Container(
-          color: Colors.white,
-          child: _buildList(),
-        ));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Section("Add a Resource to this App","Description"),
+              Section("Volunteer in Your Community","Description"),
+              Section("Inspirations","One liner"),
+            ]),
+    );
   }
 
-  final List<int> materialIndices = [900, 800, 700, 600, 500, 400, 300, 200, 100, 50];
-
-  Widget _buildList() {
-    return ListView.builder(
-        itemCount: materialIndices.length,
-        itemBuilder: (BuildContext content, int index) {
-          int materialIndex = materialIndices[index];
-          return Container(
-            color: color[materialIndex],
-            child: ListTile(
-              title: Text('$materialIndex', style: TextStyle(fontSize: 24.0)),
-              trailing: Icon(Icons.chevron_right),
-              onTap: () => onPush(materialIndex),
-            ),
-          );
-        });
-  }
 }
