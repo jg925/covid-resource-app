65d1cb5b3021946769bc4dfbd9b4734e35e3a065
Jinny Gui <jg925@cornell.edu>
2020-07-16 13:29:01 -0400

    initial contribute branch commit

---
+++ lib/screens/contribute/contribute.dart
@@ -0,0 +1,22 @@
+// screens/contribute/contribute.dart
+import 'package:flutter/material.dart';
+import 'section.dart';
+
+class Contribute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ways You Can Help'),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextSection(Colors.red),
+            TextSection(Colors.green),
+            TextSection(Colors.blue),
+          ]),
+    );
+  }
+}
--- lib/screens/resources/resources.dart
+++ lib/screens/resources/resources.dart
@@ -1,4 +1,4 @@
-// screens/Resources/Resources.dart
+// screens/resources/resources.dart
 
 import 'package:flutter/material.dart';
 
