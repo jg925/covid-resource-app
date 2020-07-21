a13641bf572745da8bb0a22a316b2c54f7d4d03f
Michelle Cheng <miche2lec@gmail.com>
2020-07-21 14:05:41 -0700

    Added update function to bottomSheet based on dropdown menu and sample ListView widget

--- lib/screens/resources/resources.dart
+++ lib/screens/resources/resources.dart
@@ -22,6 +22,8 @@ class Resources extends StatefulWidget {
 
 class ResourcesState extends State<Resources> {
   GoogleMapController mapController;
+  double bottomSheetSize = 250;
+  double bottomSheetMinSize = 0;
   
   final LatLng _center = const LatLng(45.521563, -122.677433);
   void _onMapCreated(GoogleMapController controller) {
@@ -30,17 +32,90 @@ class ResourcesState extends State<Resources> {
 
   List<DropdownMenuItem<ResourcesListItem>> _resourcesDropdownMenuItems;
   ResourcesListItem _selectedItem;
+  Column _bottomSheetContent;
   
 
   void initState() {
     super.initState();
     _resourcesDropdownMenuItems = buildResourcesDropdownMenuItems();
+    _bottomSheetContent = getInitialBottomSheetContent();
+
+  }
+
+  Column getInitialBottomSheetContent() {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Spacer(flex: 3,),
+          DropdownButton<ResourcesListItem>(
+            value: _selectedItem,
+            items: _resourcesDropdownMenuItems,
+            hint: Text("Select a category"),
+            onChanged: (value) {
+              setState(() {
+                _selectedItem = value;
+                bottomSheetSize = 500;
+                bottomSheetMinSize = 250;
+                _bottomSheetContent = getUpdatedBottomSheetContent(value);
+            });
+          }),
+          Spacer(flex: 2,),
+          SuggestionListItem(Icon(Icons.star_border), "Choose a saved place"),
+          Spacer(flex: 1,),
+          Divider(indent: 70, endIndent: 70, thickness: 1,),
+          Spacer(flex: 1,),
+          SuggestionListItem(Icon(Icons.history), "Choose from recents"),
+          Spacer(flex: 3,),
+        ]
+    );
+  }
+
+  final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100, 600, 500, 100, 600, 500, 100, 600, 500, 100, 600, 500, 100];
+
+
+
+  Column getUpdatedBottomSheetContent(value) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 20,),
+          DropdownButton<ResourcesListItem>(
+              value: _selectedItem,
+              items: _resourcesDropdownMenuItems,
+              hint: Text("Select a category"),
+              onChanged: (value) {
+                setState(() {
+                  _selectedItem = value;
+                  _bottomSheetContent = getUpdatedBottomSheetContent(value);
+                });
+              }),
+        SizedBox(height: 20,),
+        Expanded(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          )
+        ),
+
+        ]
+    );
 
   }
 
 
 
-  int _value = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +127,7 @@ class ResourcesState extends State<Resources> {
           backgroundColor: widget.color,
         ),
         body: ExpandableBottomSheet(
+          persistentContentHeight: bottomSheetMinSize,
           background: GoogleMap(
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
@@ -67,33 +143,9 @@ class ResourcesState extends State<Resources> {
             ),
           ),
           expandableContent: Container(
-            height: 250,
+            height: bottomSheetSize,
             color: Colors.white,
-            child:  Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Spacer(flex: 3,),
-                    DropdownButton<ResourcesListItem>(
-                        value: _selectedItem,
-                        items: _resourcesDropdownMenuItems,
-                        hint: Text("Select a category"),
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedItem = value;
-                          });
-                        }),
-                    Spacer(flex: 2,),
-                    SuggestionListItem(Icon(Icons.star_border), "Choose a saved place"),
-                    Spacer(flex: 1,),
-                    Divider(indent: 70, endIndent: 70, thickness: 1,),
-                    Spacer(flex: 1,),
-                    SuggestionListItem(Icon(Icons.history), "Choose from recents"),
-                    Spacer(flex: 3,),
-                  ]
-
-            ),
-
+            child:  _bottomSheetContent
           ),
        )
     );
