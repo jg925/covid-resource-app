47331de081cd227594569f92fd1b44fb1438048e
Tyler Anatole <tylerana@buffalo.edu>
2020-07-24 17:10:51 -0400

    Contents of Profile page include background, profile image and name

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 class Profile extends StatelessWidget {
   Profile({this.color, this.title, this.onPush});
-
   final MaterialColor color;
   final String title;
   final ValueChanged<int> onPush;
@@ -10,17 +11,46 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Profile Page",
-          ),
-          backgroundColor: color,
+        body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.blueAccent,Colors.indigoAccent],
+              )
+            ),
+            child:Container(
+              width:double.infinity,
+              height:350.0,
+              child:Center(
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage("Katherine-Johnson.jpg"
+                    ),
+                    radius:50.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Katherine Johnson",
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.blue
+                      ),
+                    ),
+                  ],
+                )
+              )
+            )
+          )
+        ],
         ),
-        body: Container(
-          color: Colors.white,
-        ));
+    );
   }
-
-
-
 }
